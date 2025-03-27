0238ba057bf2475934da2129ff4b4826118e1375
emowbaik <alamsantoso.12@gamil.com>
2025-03-27 16:03:54 +0800

    finally

--- arrayClass1.cpp
+++ arrayClass1.cpp
@@ -1,7 +1,33 @@
 #include <iostream>
 using namespace std;
+string kelas[30];
+int n = 0;
+void input(string nama);
+void output();
 
 int main() {
-    cout << "Hello World!";
+    input("Decy Alam Santoso");
+    output();
+    input("Rio ganteng");
+    output();
+    input("Fajri dan Dzaki apalah coba");
+    output();
+
     return 0;
+}
+
+void input(string nama) {
+    kelas[n] = nama;
+    n++;
+}
+
+void output() {
+    for (int i = 0; i < n; i++) {
+        cout << kelas[i];
+        if (i < n - 1) {
+            cout <<",";
+        } else {
+            cout <<"."<<endl;
+        }
+    }
 }
